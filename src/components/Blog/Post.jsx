b7f0cfe1a5691ecceb4ddf8ee8c6999f8f36b312
pauloprsdesouza <paulo.prsdesouza@gmail.com>
2021-12-05 22:44:55 -0300

    Add face comments async

--- src/components/Blog/Post.jsx
+++ src/components/Blog/Post.jsx
@@ -5,20 +5,28 @@ import { posts } from '../../Database/Posts';
 function Post() {
     const { id } = useParams();
     const [post, setPost] = useState({});
-
+    const [teste, setTeste] = useState(bla());
+    
 
     useEffect(() => {
         const postById = posts.find(item => item.id == id);
-
+        setTeste(bla());
         setPost(postById);
     }, [id])
+
+
+    function bla() {
+        return (
+            <div className="fb-comments" data-href={`https://paulosouza.me/blog/${id}`} data-width="100%" data-numposts="10"></div>
+        )
+    }
 
     return (
         <>
             {post.title}
             <div className="card">
                 <div className="card-body">
-                    <div className="fb-comments" data-href={`https://paulosouza.me/blog/${id}`} data-width="100%" data-numposts="10"></div>
+                    {teste}
                 </div>
             </div>
         </>
